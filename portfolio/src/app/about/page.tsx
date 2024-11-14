3ad65f493ab0b59da07ccba75d3e5b8a281702ec
Lunario99 <110005188+Lunario99@users.noreply.github.com>
2024-11-14 21:04:26 +1100

    Setting up  Next.js

---
+++ portfolio/src/app/about/page.tsx
@@ -0,0 +1,11 @@
+export default function About(){
+    return(
+        <main>
+            <h2>About</h2>
+            <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. 
+                Repudiandae tempore sunt officiis cumque distinctio. Quidem 
+                optio corporis quod laudantium omnis ipsum esse consequatur expedita, 
+                magni error, repellendus deleniti, facilis sit!</p>
+        </main>
+    )
+}
