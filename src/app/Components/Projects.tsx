6bbd93eb96b3a632bd7ce89cbba64a92837384ed
Lunario99 <110005188+Lunario99@users.noreply.github.com>
2024-11-18 18:55:40 +1100

    Updating and adding links

--- src/app/Components/Projects.tsx
+++ src/app/Components/Projects.tsx
@@ -22,41 +22,12 @@ export default function Project({
 
   return (
     <div
-      className="card mb-3 border-dark rounded-t-lg shadow transition ease-in-out 
+      className="mb-3 border-dark bg-dark rounded shadow transition ease-in-out 
                     delay-150 hover:-translate-y-1 hover:scale-102 duration-300"
     >
-      <div className="card-body row bg-dark text-white m-0">
+      <div className="row text-white m-0">
         <div className="col-7">
-          <h2 className="mb-3">{title}</h2>
-          <button className="flex flex-col">
-            <svg
-              xmlns="http://www.w3.org/2000/svg"
-              x="0px"
-              y="0px"
-              width="100"
-              height="100"
-              viewBox="0,0,256,256"
-            >
-              <g
-                fill="#ffffff"
-                stroke="none"
-                stroke-width="1"
-                stroke-linecap="butt"
-                stroke-linejoin="miter"
-                stroke-miterlimit="10"
-                stroke-dasharray=""
-                stroke-dashoffset="0"
-                font-family="none"
-                font-weight="none"
-                font-size="none"
-                text-anchor="none"
-              >
-                <g transform="scale(5.33333,5.33333)">
-                  <path d="M11.5,6c-3.01977,0 -5.5,2.48023 -5.5,5.5v25c0,3.01977 2.48023,5.5 5.5,5.5h25c3.01977,0 5.5,-2.48023 5.5,-5.5v-25c0,-3.01977 -2.48023,-5.5 -5.5,-5.5zM11.5,9h25c1.39823,0 2.5,1.10177 2.5,2.5v25c0,1.39823 -1.10177,2.5 -2.5,2.5h-25c-1.39823,0 -2.5,-1.10177 -2.5,-2.5v-25c0,-1.39823 1.10177,-2.5 2.5,-2.5zM15.5,13c-1.38071,0 -2.5,1.11929 -2.5,2.5c0,1.38071 1.11929,2.5 2.5,2.5c1.38071,0 2.5,-1.11929 2.5,-2.5c0,-1.38071 -1.11929,-2.5 -2.5,-2.5zM14,20c-0.553,0 -1,0.447 -1,1v13c0,0.553 0.447,1 1,1h3c0.553,0 1,-0.447 1,-1v-13c0,-0.553 -0.447,-1 -1,-1zM21,20c-0.553,0 -1,0.447 -1,1v13c0,0.553 0.447,1 1,1h3c0.553,0 1,-0.447 1,-1v-7.5c0,-1.379 1.121,-2.5 2.5,-2.5c1.379,0 2.5,1.121 2.5,2.5v7.5c0,0.553 0.447,1 1,1h3c0.553,0 1,-0.447 1,-1v-8c0,-3.309 -2.691,-6 -6,-6c-1.538,0 -2.937,0.58602 -4,1.54102v-0.54102c0,-0.553 -0.447,-1 -1,-1z"></path>
-                </g>
-              </g>
-            </svg>
-          </button>
+          <h2 className="mt-2 mb-2">{title}</h2>
           <ul>
             <li>Type: {type}</li>
             <li>Role: {role} </li>
@@ -65,12 +36,12 @@ export default function Project({
           <div
             className={`overflow-hidden transition-all duration-500 ease-in-out ${
               isExpanded
-                ? "max-h-[500px] opacity-100"
+                ? "max-h-auto opacity-100 sm:"
                 : "max-h-0 opacity-0 delay-75"
             }`}
           >
             {isExpanded && (
-              <div className=" flex-wrap p-3 bg-dark text-white">
+              <div className=" flex-wrap bg-dark text-white">
                 {description && (
                   <p className="mt-3 text-gray-300">{description}</p>
                 )}
@@ -78,7 +49,7 @@ export default function Project({
             )}
           </div>
         </div>
-        <div className="col-5">
+        <div className="p-2 col-4">
           <img
             src={image}
             alt=""
