ed081da63e2ca211a365c398f6620f1b8f62850a
Lunario99 <daniele.moretti.987@gmail.com>
2024-11-20 10:08:28 +1100

    Images

--- src/app/Components/GenericCarousel.tsx
+++ src/app/Components/GenericCarousel.tsx
@@ -5,7 +5,7 @@ export default function GenericCarousel({ gallery }: { gallery: string[] }) {
     <Carousel className="md:w-[40%]" controls={false}>
       {gallery.map((img, index) => (
         <Carousel.Item key={index}>
-          <img src={img} className="" />
+          <img src={img} className="w-100" />
         </Carousel.Item>
       ))}
     </Carousel>
