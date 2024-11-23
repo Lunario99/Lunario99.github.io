9f1048a87976b9296a0721ac40f5870c3f4029f2
Lunario99 <110005188+Lunario99@users.noreply.github.com>
2024-11-24 09:33:42 +1100

    images

--- src/app/Components/GenericCarousel.tsx
+++ src/app/Components/GenericCarousel.tsx
@@ -3,7 +3,7 @@ import { Carousel } from "react-bootstrap";
 
 export default function GenericCarousel({ gallery }: { gallery: string[] }) {
   return (
-    <Carousel className="w-[40%] pb-2" controls={false}>
+    <Carousel className="md:w-[40%] pb-2" controls={false}>
       {gallery.map((img, index) => (
         <Carousel.Item key={index}>
           <img src={img} className="w-100 rounded h-100" />
