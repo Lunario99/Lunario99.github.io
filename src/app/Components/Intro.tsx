86dbbbf71911900409b4b235b5f05b3d53a9671f
Lunario99 <daniele.moretti.987@gmail.com>
2024-11-24 00:06:34 +1100

    Hydration fix trying

--- src/app/Components/Intro.tsx
+++ src/app/Components/Intro.tsx
@@ -1,4 +1,3 @@
-"use client";
 export default function Intro({
   name,
   description,
--- src/app/Components/Projects.tsx
+++ src/app/Components/Projects.tsx
@@ -1,4 +1,3 @@
-"use client";
 import GenericCarousel from "./GenericCarousel";
 
 export default function Project({
